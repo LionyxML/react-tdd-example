7f6f9a60cd11bae2d0f2c522ffa5a364ca870eeb
LionyxML <rahul.juliato@gmail.com>
2022-03-01 19:25:36 -0300

    feat: adds a test to check if number of comments is equal to number of items passed into CommentFeed.

--- app/src/components/containers/CommentFeed.test.js
+++ app/src/components/containers/CommentFeed.test.js
@@ -6,11 +6,17 @@ import { render } from "@testing-library/react";
 import CommentFeed from "./CommentFeed";
 
 describe("CommentFeed", () => {
-  const props = { header: "Comment Feed" };
+  const props = { header: "Comment Feed", comments: [] };
 
   it("renders the CommentFeed component", () => {
     const { queryByText } = render(<CommentFeed {...props} />);
     const header = queryByText("Comment Feed");
     expect(header.innerHTML).toBe(props.header);
+  });
+
+  it("renders the comment list", () => {
+    const { container } = render(<CommentFeed {...props} />);
+    const commentNodes = container.querySelectorAll(".Comment");
+    expect(commentNodes.length).toBe(props.comments.length);
   });
 });
